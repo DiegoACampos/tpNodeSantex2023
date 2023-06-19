e64ffe4e5258f8aa3c11298152ee26c22e898bc2
DiegoACampos <99999011+DiegoACampos@users.noreply.github.com>
2023-06-19 12:12:25 -0300

    Update library-controller.js

--- src/controllers/library-controller.js
+++ src/controllers/library-controller.js
@@ -1,63 +1,46 @@
-const libraryService = require('../services/library-service');
+const { Book } = require('../models/books');
+const libraryService = require('../service/libraries-service');
 
-async function createLibrary(req, res, next) {
-  const { name, location, telephone } = req.body;
-
-  try {
-    const library = await libraryService.createLibrary(name, location, telephone);
-    res.status(201).send(library);
-  } catch (error) {
-    next(error);
-  }
+async function createLibrary(req, res) {
+    const { nombre, location, telefono } = req.body;
+        const libraryCreated = await libraryService.create(nombre, location, telefono)
+    res.status(201).send(libraryCreated)
 }
 
 async function getLibraryById(req, res, next) {
-  const { id } = req.params;
-
-  try {
-    const library = await libraryService.getLibraryById(id);
-    if (!library) {
-      return res.status(404).send('Librería no encontrada');
+    const { id } = req.params;
+    try {
+        const library = await libraryService.getLibrary(id)
+        res.status(200).send(library)
+    } catch (error) {
+        next(error)
     }
-    res.status(200).send(library);
-  } catch (error) {
-    next(error);
-  }
 }
 
-async function getAllLibraries(req, res, next) {
-  try {
-    const library = await libraryService.getAllLibraries();
-    res.status(200).send(library);
-  } catch (error) {
-    next(error);
-  }
+async function getAllLibraries(req, res) {
+    const listLibraries = await libraryService.getAll()
+    res.status(200).send(listLibraries)
 }
 
 async function editLibrary(req, res, next) {
-  const { id } = req.params;
-  const { name, location, telephone } = req.body;
-
-  try {
-    const library = await bookService.editLibrary(id, name, location, telephone);
-    res.status(200).send(library);
-  } catch (error) {
-    next(error);
-  }  
-}
-
-async function deleteLibrary(req, res, next) {
-  const { id } = req.params;
-
-  try {
-    const deletedLibrary = await libraryService.deleteLibrary(id);
-    if (!deletedLibrary) {
-      return res.status(404).send('Librería no encontrada');
+    const { id } = req.params;
+    const { nombre, location, telefono } = req.body;
+    try {
+        const editedLibrary = await libraryService.edit(id, nombre, location, telefono)
+        res.status(200).send(editedLibrary)
+    } catch (error) {
+        next(error)
     }
-    res.status(200).send('Librería eliminada');
-  } catch (error) {
-    next(error);
-  }
 }
 
-module.exports = { createLibrary, getLibraryById, getAllLibraries, editLibrary , deleteLibrary };
+async function deleteOneLibrary(req, res, next) {
+    const { id } = req.params;
+    try {
+        await libraryService.deleteLibrary(id)
+        res.status(200).send(`Libreria con id:${id} y libros en su bodega an sido eliminados`)
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = { createLibrary, getLibraryById, getAllLibraries, editLibrary, deleteOneLibrary }
