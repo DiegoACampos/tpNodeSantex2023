86e10d05bdb3c2113ddcfb51785facd5b9718461
DiegoACampos <99999011+DiegoACampos@users.noreply.github.com>
2023-06-19 12:21:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,27 @@
 const express = require('express');
-
-const bodyParser = require('body-parser');
-const libraryRoutes = require('./src/routes/library-routes');
-const bookRoutes = require('./src/routes/book-routes');
-const userRoutes = require('./src/routes/user-routes');
-
-const { errorHandlerMiddleware } = require('./src/middleware/error-handler');
-const { Libraries } = require( './src/models/librarys' )
-const { liveBooks } = require('./src/models/book');
-const { libUser } = require('./src/models/user');
+const bookRoutes = require('./src/routes/books-routes')
+const librariesRoutes = require('./src/routes/libraries-routes');
+const userRoutes = require('./src/routes/user-routes')
+const { errorHandlerMiddleware } = require('./src/middleware/error-handler')
+const {Book} = require('./src/models/books')
+const {LibraryM} = require('./src/models/libraries')
+const {User} = require('./src/models/user')
 
 const app = express();
+const PORT = 5000;
 
-app.use(bodyParser.json());
-app.use("/library", libraryRoutes);
-app.use('/books', bookRoutes);
-app.use('/user', userRoutes); 
-app.use(errorHandlerMiddleware);
-
-Libraries.sync();
-liveBooks.sync();
-libUser.sync();
+LibraryM.sync();
+Book.sync();
+User.sync();
 
 
-const port = 5000;
-app.listen(port, () => {
-  console.log(`Servidor corriendo en puerto ${port}`);
+app.use(express.json())
+app.use('/library', librariesRoutes)
+app.use('/book', bookRoutes)
+app.use('/user', userRoutes)
+app.use(errorHandlerMiddleware)
 
-});
-  
+
+app.listen(PORT, () => {
+    console.log(`App corriendo en puerto ${PORT}`)
+})
