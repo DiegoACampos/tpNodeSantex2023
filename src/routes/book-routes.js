b1862bd213a7fee0fe7565d5cf126e3d3518a1fa
DiegoACampos <99999011+DiegoACampos@users.noreply.github.com>
2023-06-19 12:18:11 -0300

    Update book-routes.js

--- src/routes/book-routes.js
+++ src/routes/book-routes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const bookController = require('../controllers/book-controller');
-
+const bookController = require('../controller/book-controller')
 const router = express.Router();
 
-router.post('/create', bookController.createBook);
-router.get('/getById/:id', bookController.getBookById);
-router.get('/getAll', bookController.getAllBooks);
-router.put('/modify/:id',bookController.editBook);
-router.delete('/delete/:id', bookController.updateBookDeleteStatus);
+router.post('/createBook', bookController.createBook)
+router.get('/getBook/:id', bookController.getBookById)
+router.get('/getAllBooks', bookController.getAllBooks)
+router.put('/editBook', bookController.editBook)
+router.delete('/deleteBook/:id', bookController.deleteBook)
 
 module.exports = router;
